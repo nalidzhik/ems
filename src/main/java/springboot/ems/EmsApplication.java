9bbe0dba88bcea1c22e16956df9a9c892cc1ad43
Nazife Alidzhik <nazzalidzhikk@gmail.com>
2022-07-17 12:37:58 +0300

    Add frontend to the app

--- src/main/java/springboot/ems/EmsApplication.java
+++ src/main/java/springboot/ems/EmsApplication.java
@@ -3,9 +3,11 @@ package springboot.ems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmsApplication {
 
 	public static void main(String[] args) {
